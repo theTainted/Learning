c84f399d4c0cf3fcc9bad472162da5c1b8db3686
syam <syam.s@outlook.com>
2015-12-09 09:02:36 +0530

    Updated to check stalelement exception, using explicit wait. Using expectedconditions.presenceofelementlocated

--- src/test/java/com/learning/tests/TestCaseTestNg.java
+++ src/test/java/com/learning/tests/TestCaseTestNg.java
@@ -1,16 +1,20 @@
 package com.learning.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pageobjects.HomePage;
 import pageobjects.ProductDisplayPage;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -46,7 +50,7 @@ public class TestCaseTestNg {
     public void testClickOnSignIn () throws Exception {
         driver.navigate().to("http://jv-dev.dev-tzr.eu/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 
@@ -57,15 +61,13 @@ public class TestCaseTestNg {
        homePage.enterSearchText("product");
 
         homePage.clickSearchButton(driver,"product");
-        Thread.sleep(5000);
+       // Thread.sleep(5000);
         //homePage.hoverOverMiniBasket();
         ProductDisplayPage PDP = PageFactory.initElements(driver,ProductDisplayPage.class);
         PDP.setSize("product");
         PDP.clickOnAddToBasket(driver);
-        homePage.hoverOverMiniBasket(driver);
+        //homePage.hoverOverMiniBasket(driver);
 
-
-        //System.out.println(sText);
     }
 
 }
